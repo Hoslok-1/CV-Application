b4d91f122712ebca71ff1e81ed004aa16fbb2a21
Hoslok-1 <anujhoslokdev@gmail.com>
2023-04-14 11:24:33 +0530

    initial configuration with TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  let num = 10;
+  return(
+    <div>
+      <p>Hello,World {num}</p>
+    </div>
+  )
+}
+
+export default App;
